0723c0cbe501dbd9b0bd2f64b115943b4e6b0506
Sergei K <ascirex@gmail.com>
2020-09-01 03:40:08 +0300

    Try add .gitignore

--- NewHomeWork1.4/Program.cs
+++ NewHomeWork1.4/Program.cs
@@ -14,9 +14,8 @@ namespace NewHomeWork1._4
 
             var selector = new Selector();
                 selector.Begin();
-             
 
-            #region END PROGRAM 
+                #region END PROGRAM 
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\n The end.\n  До скорого :)\n");
             Console.ReadKey();
