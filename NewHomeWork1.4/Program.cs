e05c677e21e7cbf7a3c3484ab635c4ac4a2bb8c9
Sergei K <ascirex@gmail.com>
2020-09-01 03:49:45 +0300

    йц

--- NewHomeWork1.4/Program.cs
+++ NewHomeWork1.4/Program.cs
@@ -15,6 +15,7 @@ namespace NewHomeWork1._4
             var selector = new Selector();
                 selector.Begin();
 
+
                 #region END PROGRAM 
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\n The end.\n  До скорого :)\n");
