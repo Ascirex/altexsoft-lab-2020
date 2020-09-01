7445d8f2dfd4ef70b683a1b8c8fb658773129783
Sergei K <ascirex@gmail.com>
2020-09-01 03:59:26 +0300

    Add project with gitignore

---
+++ NewHomeWork1.4/ConsoleExt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace NewHomeWork1._4
+{
+    internal static class ConsoleExt
+    { 
+        //Тут банальные врайтлайны с измененным цветом текста
+
+        public static void WriteLine(string text, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.White;
+        } 
+
+        public static void Write(string text, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+        public static void WriteSelect(int x, int maxPaddingLength = 7)
+        {
+            //на всякий, максимальное количество нолей в индексе селектора
+            var padding = maxPaddingLength - x.ToString().Length;
+            Write("[".PadLeft(padding + 1));
+            Write(x.ToString(), ConsoleColor.Cyan); Write("] ");
+
+        }
+
+        public static void Clear()
+        {
+            Console.Clear();
+        }
+    }
+}
---
+++ NewHomeWork1.4/Methods/Models/ReverceText.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NewHomeWork1._4.Methods.Models
+{
+    internal class ReverceText
+    {
+        public void Reverce(string pathF)
+        {
+            Console.Clear();
+            var ReadyText = FileTextReader.TextReader(pathF);
+
+            var textReverce = ReadyText.Split(new char[] { '?', '!', '.' }, StringSplitOptions.RemoveEmptyEntries);
+            var endSymbolIndex = ReadyText.LastIndexOf(textReverce[2]) - 1;
+            var lastSymbol = ReadyText[endSymbolIndex];
+            var a = Regex.Replace(textReverce[2], $@"\w(?<!\d)[\w'-]*", ReverceMatched);
+            Console.WriteLine(a + lastSymbol);
+
+            Console.ReadKey();
+
+        }
+        string ReverceMatched(Match x)
+        {
+            var xRevers = new string(x.Value.Reverse().ToArray());
+            return xRevers;
+        }
+    }
+}
